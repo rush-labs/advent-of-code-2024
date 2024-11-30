071c645eac10d110cd4afc176954989cc313da19
botre <git@bjornkrols.com>
2024-11-30 14:04:25 +0100

    Setup tools

---
+++ bjorn/README.md
@@ -0,0 +1,9 @@
+# Bjorn
+
+## Script
+
+Generate requirements file:
+
+```bash
+uv pip freeze > requirements.txt
+```
---
+++ bjorn/hello.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.DataFrame({"name": ["John", "Jane", "Sue", "Fred"], "age": [23, 29, 21, 18]})
+
+print(df)
