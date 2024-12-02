81c88baf03b8a2d1c218afe5ace5567f2a9cfb63
Tim <tim@xorob.io>
2024-12-02 10:45:57 +0100

    day 2 part 1

--- tim/Sources/main.swift
+++ tim/Sources/main.swift
@@ -4,3 +4,7 @@
 print("=== Day 1 ===")
 print("solution 1", Day1().solvePart1())
 print("solution 2", Day1().solvePart2())
+
+print("=== Day 2 ===")
+print("solution 1", Day2().solvePart1())
+//print("solution 2", Day2().solvePart2())
