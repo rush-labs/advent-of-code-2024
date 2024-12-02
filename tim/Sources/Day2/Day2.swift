d66368260f038c38195dd066945675e5fccd26ef
Tim <tim@xorob.io>
2024-12-02 10:46:28 +0100

    day 2 part 1

--- tim/Sources/Day2/Day2.swift
+++ tim/Sources/Day2/Day2.swift
@@ -1,57 +1,53 @@
 import Foundation
 
-struct Lanes {
-    let leftLane: [Int]
-    let rightLane: [Int]
+enum Status {
+    case Safe
+    case Unsafe
 }
 
-struct Day1 {
-    func readInputFile() -> Lanes {
-        guard let input = try? String(contentsOfFile: "Sources/Day1/input.txt") else {
+struct Day2 {
+    func readInputFile() -> [[Int]] {
+        guard let input = try? String(contentsOfFile: "Sources/Day2/input.txt") else {
             fatalError("Could not read file")
         }
         let lines = input.components(separatedBy: .newlines)
-        let leftLane = lines.map { line in
-            let numbers = line.split(separator: " ")
-            guard numbers.count > 0 else {
-                return 0
-            }
-            return Int(numbers[0]) ?? 0
-        }.sorted()
 
-        let rightLane = lines.map { line in
-            let numbers = line.split(separator: " ")
-            guard numbers.count > 1 else {
-                return 0
-            }
-            return Int(numbers[1]) ?? 0
-        }.sorted()
-        return Lanes(leftLane: leftLane, rightLane: rightLane)
+        let floors: [[Int]] = lines.map { line in
+            return line.split(separator: " ").map { numberString in Int(numberString) ?? 0}
+        }.filter { floor in floor != []}
+
+
+        return floors
     }
+
     func solvePart1() -> String {
-        let input = readInputFile()
+        let floors = readInputFile()
 
+        let status: [Status] = floors.map { floor in
+            if(floor.sorted() != floor && floor.sorted(by: >) != floor){
+                return Status.Unsafe
+            }
+            return floor.enumerated().reduce(Status.Safe) { (s, tuple) in
+                let (index, pos) = tuple
 
-        let distances = input.leftLane.enumerated().map { (index, leftNumber) in
-            return abs(leftNumber - input.rightLane[index])
+                guard index > 0 else { return s}
+
+                if s == .Unsafe {
+                    return .Unsafe
+                }
+
+                let diff = abs(pos - floor[index-1])
+
+                if diff > 3 || diff < 1 {
+                    return .Unsafe
+                }
+
+                return .Safe
+
+            }
         }
 
-        let totalDistance = distances.reduce(0) { (total, distance) in
-            return  total + distance
-        }
-
-        return String(totalDistance)
+        return String(status.filter { s in s == Status.Safe}.count)
     }
-    func solvePart2() -> String {
-        let input = readInputFile()
 
-        let similarityScore = input.leftLane.reduce(0) { ( total, leftNumber) in
-            return total + (input.rightLane.filter { rightNumber in
-              rightNumber == leftNumber
-             }.count * leftNumber)
-        }
-
-
-        return String(similarityScore)
-    }
 }
