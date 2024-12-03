fbf0e5ca0b9e17ed8ea7bfdce278b3ee42f12701
toc <tomvarewyck98@gmail.com>
2024-12-03 17:24:44 +0100

    🐛 Day 3 | Part 1

--- tom/day-3/main.rb
+++ tom/day-3/main.rb
@@ -1,7 +1,6 @@
 require_relative '../utils'
 
 def mul(x, y)
-  puts "#{x} * #{y} = #{x.to_i * y.to_i}"
   x.to_i * y.to_i
 end
 
@@ -9,17 +8,21 @@ end
 def mul_scanner(filename)
   factors = []
   File.foreach(filename) do |line|
-    factors = line.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+    lines = line.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+    factors << lines
   end
   factors
 end
 
-def part_one
-  factors = mul_scanner("tom/day-3/example.txt")
-  print factors
-  puts ''
-  total = factors.sum{|factor| mul(factor[0], factor[1]) }
-  puts total
+def part_one(file_name)
+  factors = mul_scanner(file_name)
+  total = factors.sum{|line| line.sum{|factor| mul(factor[0], factor[1]) }}
+  puts "Total: #{total}"
 end
 
-part_one
+def part_two(file_name)
+
+end
+
+part_one("tom/day-3/example.txt")
+part_one("tom/day-3/inputs.txt")
